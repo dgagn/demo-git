9cfdec16ad1ce1531e39524bcbefa198534f1a5f
Dany Gagnon <dany@ovior.ca>
2026-02-17 14:53:46 -0500

    ajout auhtentification

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+int is_auth() {
+    return 1;
+}
+
 int main(void) {
     printf("Hello World 3");
     printf("troisième modification");
     printf("on est sur develop");
+    if (is_auth() == 1) {
+        return 1;
+    }
     return 0;
 }
